673442f87f8d893b3ccab781c0749c664a50a564
David Barnes <david.barnes0402@gmail.com>
2020-11-09 16:59:14 -0500

    Bare bones inventory class. Produces a member, but no methods implemented

---
+++ Store/Store.Library/Inventory.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Store.Library
+{
+    public class Inventory
+    {
+        // backing field for "ProductId"
+        private int _productId;
+        // backing field for "Quantity"'
+        private int _quantity;
+
+        /// <summary>
+        /// The Product Id
+        /// </summary>
+        public int ProductId
+        {
+            get { return _productId; }
+            private set
+            {
+                //make sure that it is greater than zero
+                if (value > 0)
+                    _productId = value;
+                else
+                    throw new ArgumentOutOfRangeException("ProductId", "The product ID must be a value greater than zero");
+            }
+        }
+
+        /// <summary>
+        /// The quantity in stock at a location
+        /// </summary>
+        public int Quantity { 
+            get { return _quantity; }
+            private set
+            {
+                if (value >= 0)
+                    _quantity = value;
+                else
+                    throw new ArgumentOutOfRangeException("Quantity", "The Quantity must be a value greater or equal to zero");
+            }
+        }
+
+        /// <summary>
+        /// Constructor for the Inventory
+        /// Will default to zero inventory
+        /// </summary>
+        /// <param name="id">Product Id</param>
+        public Inventory(int id)
+        {
+            this.ProductId = id;
+            this.Quantity = 0;
+        }
+
+        public void AddInventory(int quantity)
+        {
+            throw new NotImplementedException("Not implemented");
+        }
+    }
+}
