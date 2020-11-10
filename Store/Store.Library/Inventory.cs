5ba052ed1bc37ce626a6b2ae8906b75796398ded
David Barnes <david.barnes0402@gmail.com>
2020-11-10 09:30:56 -0500

    Add a Product field to Inventory to store the actual product not just the id

--- Store/Store.Library/Inventory.cs
+++ Store/Store.Library/Inventory.cs
@@ -10,6 +10,15 @@ namespace Store.Library
         private int _productId;
         // backing field for "Quantity"'
         private int _quantity;
+        // backing field for "Product"
+        // private Product _productObj;
+
+
+        // Store an actual product instead of just an id,
+        // If we store just an id here we will need a database of
+        // products to query and we don't have that yet
+        // will leave in productId for future implementations
+        // that will have a database to link to.
 
         /// <summary>
         /// The Product Id
@@ -30,7 +39,8 @@ namespace Store.Library
         /// <summary>
         /// The quantity in stock at a location
         /// </summary>
-        public int Quantity { 
+        public int Quantity 
+        { 
             get { return _quantity; }
             private set
             {
@@ -40,6 +50,8 @@ namespace Store.Library
                     throw new ArgumentOutOfRangeException("Quantity", "The Quantity must be a value greater or equal to zero");
             }
         }
+
+        public Product ProductObj { get; set; }
 
         /// <summary>
         /// Constructor for the Inventory
@@ -54,12 +66,40 @@ namespace Store.Library
 
         /// <summary>
         /// Constructor for the inventory
+        /// With only a product ID
         /// </summary>
         /// <param name="id">Product Id</param>
         /// <param name="quantity">Initial Quantity</param>
         public Inventory(int id, int quantity)
         {
             this.ProductId = id;
+            this.Quantity = quantity;
+        }
+
+        /// <summary>
+        /// Constructor for the Inventory
+        /// With a Product object being passed in
+        /// </summary>
+        /// <remarks>
+        /// Since this constructor doesn't take in any arguments in regards to quantity,
+        /// it will assume that there is no quantity and set it to zero
+        /// </remarks>
+        /// <param name="prod">The Product Object</param>
+        public Inventory(Product prod)
+        {
+            this.ProductObj = prod;
+            this.Quantity = 0;
+        }
+
+        /// <summary>
+        /// Constructor for Inventory
+        /// With Product object and a Quantity
+        /// </summary>
+        /// <param name="prod">The Product Object</param>
+        /// <param name="quantity">How many in Inventory</param>
+        public Inventory(Product prod, int quantity)
+        {
+            this.ProductObj = prod;
             this.Quantity = quantity;
         }
 
