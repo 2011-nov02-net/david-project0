ef7133f0006b5ee11563ec974bfe7b28dc0ce6bd
David Barnes <david.barnes0402@gmail.com>
2020-11-10 13:24:19 -0500

    Class to store order information

---
+++ Store/Store.Library/Order.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Store.Library
+{
+    public class Order
+    {
+        public int CustomerId { get; set; }
+        public int LocationId { get; set; }
+        public ICollection<Sale> SalesList { get; set; }
+        public DateTime Date { get; set; }
+
+        public decimal GetTotalOfOrder()
+        {
+            return SalesList.Sum(sale => sale.ProductObj.Price * sale.SaleQuantity);
+        }
+
+        public Order(int custId, int locId, List<Sale> sales, DateTime date)
+        {
+            this.CustomerId = custId;
+            this.LocationId = locId;
+            this.SalesList = sales;
+            this.Date = date;
+        }
+    }
+}
