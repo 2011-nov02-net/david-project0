b52aa09a66b6b7916ae70702711b38e07d85cb6f
David Barnes <david.barnes0402@gmail.com>
2020-11-10 13:25:07 -0500

    A sale class to keep track of the product that is ordered and the quantity

---
+++ Store/Store.Library/Sale.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Store.Library
+{
+    public class Sale
+    {
+        public Product ProductObj { get; set; }
+        public int SaleQuantity
+        {
+            get { return SaleQuantity; }
+            set 
+            {
+                if (value > 0)
+                    SaleQuantity = value;
+                else
+                    throw new ArgumentOutOfRangeException("SaleQuantity", "Sale Quantity must be greater than zero");
+           }
+        }
+
+        public Sale(Product prod, int quantity)
+        {
+            this.ProductObj = prod;
+            this.SaleQuantity = quantity;
+        }
+    }
+}
