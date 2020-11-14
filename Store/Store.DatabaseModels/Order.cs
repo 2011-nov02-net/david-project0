6b3dda60baa6f3a52679239829521ee0f4ec9f7c
David Barnes <david.barnes0402@gmail.com>
2020-11-14 17:25:33 -0500

    add db context from azure db

---
+++ Store/Store.DatabaseModels/Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Store.DatabaseModels
+{
+    public partial class Order
+    {
+        public Order()
+        {
+            Sales = new HashSet<Sale>();
+        }
+
+        public int CustomerId { get; set; }
+        public int LocationId { get; set; }
+        public DateTime Date { get; set; }
+        public int OrderNumber { get; set; }
+
+        public virtual Customer Customer { get; set; }
+        public virtual Location Location { get; set; }
+        public virtual ICollection<Sale> Sales { get; set; }
+    }
+}
---
+++ Store/Store.DatabaseModels/Sale.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Store.DatabaseModels
+{
+    public partial class Sale
+    {
+        public int OrderNumber { get; set; }
+        public int ProductId { get; set; }
+        public decimal PurchasePrice { get; set; }
+        public int Quantity { get; set; }
+
+        public virtual Order OrderNumberNavigation { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
